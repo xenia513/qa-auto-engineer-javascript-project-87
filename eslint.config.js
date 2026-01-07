b6d85bfd3645bd6c48ceaa4e43abd318ad27b276
xenia513 <xenia513@yandex.ru>
2026-01-07 16:49:30 +0400

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from "@eslint/js"
 import globals from "globals"
 import pluginReact from "eslint-plugin-react"
-import { defineConfig } from 'eslint'
+import { defineConfig } from '@eslint/js'
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
