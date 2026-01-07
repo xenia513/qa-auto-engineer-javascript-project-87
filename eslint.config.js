fad37ad3af94aaab63fbf7ef81deeb5025527d50
xenia513 <xenia513@yandex.ru>
2026-01-07 16:52:20 +0400

    Add CI/CD pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
 import js from "@eslint/js"
 import globals from "globals"
 import pluginReact from "eslint-plugin-react"
-import { defineConfig } from '@eslint/js'
+import pkg from '@eslint/js'
+const { defineConfig } = pkg
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: ["js"],
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
   pluginReact.configs.flat.recommended,
 ])
