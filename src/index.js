968b8c7cac4fe8565c69b2a4de7041575a86478c
xenia513 <xenia513@yandex.ru>
2026-01-08 02:24:30 +0400

    steps 6-8

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import getFormatter from './formatters/index.js'
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(filepath1)
   const data2 = parseFile(filepath2)
-  const diff = diffObjects(obj1, obj2)
+  const diff = diffObjects(data1, data2)
 
   const formatter = getFormatter(format)
   return formatter(diff)
