61ac771259a54dd7b75eb745d5b476c8d729ef51
xenia513 <xenia513@yandex.ru>
2026-01-09 00:57:10 +0400

    optimized tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,12 +1,9 @@
 import genDiff from '../index.js'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = process.cwd()
 
 const getFixturePath = filename =>
-  path.join(__dirname, '..', '..', '__fixtures__', filename)
+  path.join(__dirname, '__fixtures__', filename)
 
 // тесты для плоских JSON и YAML
 const expectedStylishFlat = `{
