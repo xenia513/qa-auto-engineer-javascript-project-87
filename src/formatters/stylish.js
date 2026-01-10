ef6adc5cf0370786b2be1e3024f60afc17d81561
xenia513 <xenia513@yandex.ru>
2026-01-11 03:46:31 +0400

    nested files excluded

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,18 +1,17 @@
-const stringify = (value, replacer, depth) => {
-  const iter = (currentValue, currentDepth) => {
+const stringify = (value) => {
+  const iter = (currentValue) => {
     if (typeof currentValue !== 'object' || currentValue === null) {
       return `${currentValue}`
     }
-    return String(currentValue) 
+    return String(currentValue)
   }
-  return iter(value, depth)
+  return iter(value)
 }
 
 const stylish = (tree) => {
   const iter = (node, depth) => {
     const replacer = '    '
     const bracketIndent = replacer.repeat(depth - 1)
-
     const result = Array.from(node)
       .map(({
         key, type, value,
@@ -20,13 +19,13 @@ const stylish = (tree) => {
         const contentIndent = replacer.repeat(depth - 1) + '  '
         switch (type) {
           case 'added':
-            return `${contentIndent}+ ${key}: ${stringify(value, replacer, depth + 1)}`
+            return `${contentIndent}+ ${key}: ${stringify(value)}`
           case 'removed':
-            return `${contentIndent}- ${key}: ${stringify(value, replacer, depth + 1)}`
+            return `${contentIndent}- ${key}: ${stringify(value)}`
           case 'updated':
-            return `${contentIndent}- ${key}: ${stringify(value.value1, replacer, depth + 1)}\n${contentIndent}+ ${key}: ${stringify(value.value2, replacer, depth + 1)}`
+            return `${contentIndent}- ${key}: ${stringify(value.value1)}\n${contentIndent}+ ${key}: ${stringify(value.value2)}`
           default:
-            return `${contentIndent}  ${key}: ${stringify(value, replacer, depth + 1)}`
+            return `${contentIndent}  ${key}: ${stringify(value)}`
         }
       })
     return ['{', ...result, `${bracketIndent}}`].join('\n')
