4a6cf5f0cf2507c47d5719d53cf262144ed60cb7
xenia513 <xenia513@yandex.ru>
2026-01-10 23:43:15 +0400

    first check fixes

--- eslint.config.js
+++ eslint.config.js
@@ -9,8 +9,8 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest
-      }
-    }
-  }
+        ...globals.jest,
+      },
+    },
+  },
 ])
